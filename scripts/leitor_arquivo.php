d1be9a2c339c22f163853723115bf6b9810aba45
mario-leandro <marioleandro2004@gmail.com>
2025-09-23 12:38:11 -0300

    att 2

--- scripts/leitor_arquivo.php
+++ scripts/leitor_arquivo.php
@@ -5,9 +5,6 @@ include __DIR__ . '/../common.php';
 headers();
 
 if($_FILES['file'] && $_FILES['file']['error'] === 0) {
-    $arquivo = $_FILES['file']['name'];
-
-    $pastaArquivos = __DIR__ . '/../arquivos/' . $arquivo;
 
     $infoArquivo = finfo_open(FILEINFO_MIME_TYPE);
     $tipoArquivo = finfo_file($infoArquivo, $_FILES['file']['tmp_name']);
@@ -16,7 +13,13 @@ if($_FILES['file'] && $_FILES['file']['error'] === 0) {
     // aquivos permitidos, xml, json, csv
     $tiposDeArquivos = ["application/xml", "text/xml", "application/json", "text/csv", "text/plain"];
 
+    // Verifica se o tipo de arquivo é permitido
+    if(!in_array($tipoArquivo, $tiposDeArquivos)) {
+        resposta(400, ["success" => false, "mensagem" => "Tipo de arquivo não permitido."]);
+    }
+
+    $pegarArquivos = curl_init('http://localhost:8000/api/upload');
     
 } else {
-    resposta(400, ["success" => false, "error" => "No file uploaded or upload error."]);
+    resposta(400, ["success" => false, "mensagem" => "Nenhum arquivo enviado ou erro no upload."]);
 }
